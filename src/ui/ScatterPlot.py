c1754944ad0083a8b279eaa73f7a1ea643c83bcc
Alexander Koshelev <alexander.y.koshelev@gmail.com>
2025-04-02 15:35:26 +0300

    Add label support

--- src/ui/ScatterPlot.py
+++ src/ui/ScatterPlot.py
@@ -1,42 +1,73 @@
+from itertools import cycle
+
 import pyqtgraph as pg
 import pandas as pd
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
 from PyQt6.QtCore import pyqtSlot
+from pyqtgraph import PlotWidget
 
-class ScatterPlot(QWidget):
+colormap = [
+    '#66B2FF',
+    '#FF6666',
+    '#66FF66',
+    '#FFCC66',
+    '#CC99FF',
+    '#66FFFF',
+    '#FFB266',
+    '#A6B2FF',
+    '#B2B2B2',
+    '#FF8533',
+    '#77B5FE',
+    '#F08080',
+    '#90EE90',
+    '#F4A460',
+    '#DDA0DD',
+    '#AFEEEE',
+    '#F0E68C',
+    '#D3D3D3',
+    '#ADD8E6',
+    '#98FB98',
+]
+
+class ScatterPlot(PlotWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        # Create graph widget
-        self.x = []
-        self.y = []
-        self.data = []
-        self.plot_widget = pg.PlotWidget()
-
-        # Create Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.plot_widget)
-        self.setLayout(layout)
-
-        # Create Scatter Plot Item
-        self.scatter_plot = pg.ScatterPlotItem(x=self.x, y=self.y, data=self.data,
-                                               size=10,
-                                               pen=pg.mkPen(None),
-                                               brush=pg.mkBrush('b'),
-                                               hoverable=True)
-
-        # Add Scatter Plot Item to graph
-        self.plot_widget.addItem(self.scatter_plot)
-
         # Configure plot axis
-        self.plot_widget.setLabel('bottom', 'X Axis')
-        self.plot_widget.setLabel('left', 'Y Axis')
+        self.setLabel('bottom', 'X Axis')
+        self.setLabel('left', 'Y Axis')
+        self.autoRange()
+
 
     @pyqtSlot(str, name="csvUpdate")
     def updateFromFile(self, filename):
         # Update with data from csv
         csv = pd.read_csv(filename, index_col=False)
-        self.x = csv['x']
-        self.y = csv['y']
-        self.data = csv['label']
-        self.scatter_plot.setData(x=self.x, y=self.y, data=self.data)
+        colorIt = cycle(colormap)
+
+        self.clear()
+
+        if 'label' in csv.columns:
+            grouped = csv.groupby('label')
+
+            for label, group in grouped:
+                scatter = pg.ScatterPlotItem(
+                    x=group['x'],
+                    y=group['y'],
+                    data=group.apply(lambda row: "[{}] {}".format(row['label'], row['data']), axis=1),
+                    size=10,
+                    pen=pg.mkPen(None),
+                    brush=pg.mkBrush(next(colorIt)),
+                    hoverable=True
+                )
+                self.addItem(scatter)
+        else:
+            scatter = pg.ScatterPlotItem(
+                x=csv['x'],
+                y=csv['y'],
+                data=csv['data'],
+                size=10,
+                pen=pg.mkPen(None),
+                brush=pg.mkBrush(next(colorIt)),
+                hoverable=True
+            )
+            self.addItem(scatter)
